885403fafe0a5f4c83e9a1ddb4c6b93fa6811784
Stefano Casasola <ste.casasola@gmail.com>
2024-08-26 12:12:09 +0200

    Adds fetch in componentDidMount

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,6 +11,40 @@ class MovieCard extends Component {
     isError: false,
   }
 
+  componentDidMount = () => {
+    this.fetchMovieData()
+  }
+
+  // ora creiamo una funzione in grado di utilizzare la prop "movieTitle"
+  // per avviare una ricerca su OMDB, e salvare in this.state.movieData
+  // il risultato più pertinente
+  fetchMovieData = () => {
+    fetch('http://www.omdbapi.com/?apikey=24ad60e9&s=' + this.props.movieTitle)
+      // this.props.movieTitle inizialmente è "Thor"
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Errore nella chiamata')
+        }
+      })
+      .then((data) => {
+        console.log('RISULTATI DI RICERCA', data)
+        console.log(data.Search[0]) // il primo risultato, il più rilevante
+        this.setState({
+          movieData: data.Search[0],
+          isLoading: false,
+        })
+      })
+      .catch((err) => {
+        console.log('ERRORE', err)
+        this.setState({
+          isLoading: false,
+          isError: true,
+        })
+      })
+  }
+
   render() {
     return (
       <>
@@ -19,17 +53,20 @@ class MovieCard extends Component {
             <Spinner animation="border" variant="success" />
           </div>
         )}
+
         {this.state.isError && (
-          <Alert variant="danger">Errore nel recupero dati</Alert>
+          <Alert variant="danger" className="text-center">
+            Errore nel recupero dati
+          </Alert>
         )}
+
         {!this.state.isLoading && !this.state.isError && (
           <Card className="text-center">
-            <Card.Img variant="top" src="https://placedog.net/300" />
+            <Card.Img variant="top" src={this.state.movieData.Poster} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>{this.state.movieData.Title}</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                {this.state.movieData.Year} - {this.state.movieData.Type}
               </Card.Text>
             </Card.Body>
           </Card>
